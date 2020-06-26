f0651cf71bd9715f0ec87d1fcb9b56e75a899d99
Ruixiang Du <ruixiang.du@westonrobot.com>
2020-06-26 10:17:48 +0800

    tested carto to map a larger area

--- hunter_cartographer/script/save_map.sh
+++ hunter_cartographer/script/save_map.sh
@@ -5,7 +5,7 @@ echo "Map name ([ENTER] to finish): "
 read mapname
 
 rosservice call /finish_trajectory 0
-rosservice call /write_state "filename: '../maps/$mapname.pbstream'
+rosservice call /write_state "filename: '/home/hunter/Workspace/catkin_ws/src/hunter_ros/hunter_cartographer/maps/$mapname.pbstream'
 include_unfinished_submaps: false" 
 
 rosrun map_server map_saver -f $mapname
